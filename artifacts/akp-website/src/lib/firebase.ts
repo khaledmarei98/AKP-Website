8e78089f4026b3129e1425aac3106b83a9e1a35d
khaledmarei98 <kmareey48@gmail.com>
2026-05-27 23:22:06 +0300

    Remove Firebase configuration and initialization
    Removed Firebase initialization and related imports.

--- artifacts/akp-website/src/lib/firebase.ts
+++ artifacts/akp-website/src/lib/firebase.ts
@@ -55,28 +55,3 @@ export const analytics = app
       .then((yes) => (yes ? getAnalytics(app) : null))
       .catch(() => null)
   : Promise.resolve(null);
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-import { getAnalytics, isSupported } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDgSFtgwcTDwvOzntael16Ys_mtp-pEyMw",
-  authDomain: "akpconsulting-dafe9.firebaseapp.com",
-  projectId: "akpconsulting-dafe9",
-  storageBucket: "akpconsulting-dafe9.appspot.com",
-  messagingSenderId: "327216395688",
-  appId: "1:327216395688:web:46e91b362b7c81106cd99d",
-  measurementId: "G-BNDG2XG2HX"
-};
-
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-
-export const analytics = isSupported()
-  .then((yes) => (yes ? getAnalytics(app) : null))
-  .catch(() => null);
