322f123559bf2173d792d6c1d6c6908f9515ce16
khaledmarei98 <kmareey48@gmail.com>
2026-05-27 21:34:14 +0300

    Update firebase.ts

--- artifacts/akp-website/src/lib/firebase.ts
+++ artifacts/akp-website/src/lib/firebase.ts
@@ -5,24 +5,21 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-   apiKey: "AIzaSyDgSFtgwcTDwvOzntael16Ys_mtp-pEyMw",
+  apiKey: "AIzaSyDgSFtgwcTDwvOzntael16Ys_mtp-pEyMw",
   authDomain: "akpconsulting-dafe9.firebaseapp.com",
   projectId: "akpconsulting-dafe9",
-  storageBucket: "akpconsulting-dafe9.firebasestorage.app",
+  storageBucket: "akpconsulting-dafe9.appspot.com",
   messagingSenderId: "327216395688",
   appId: "1:327216395688:web:46e91b362b7c81106cd99d",
   measurementId: "G-BNDG2XG2HX"
 };
-export const isConfigured = Object.entries(firebaseConfig)
-  .filter(([key]) => key !== "measurementId")
-  .every(([, v]) => Boolean(v));
 
-const app =
-  getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
-export const auth = app ? getAuth(app) : null;
-export const db = app ? getFirestore(app) : null;
-export const storage = app ? getStorage(app) : null;
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
-export const analytics = app
-  ? isSupported().then((yes) => (yes ? getAnalytics(app) : null)).catch(() => null)
-  : Promise.resolve(null);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export const analytics = isSupported()
+  .then((yes) => (yes ? getAnalytics(app) : null))
+  .catch(() => null);
